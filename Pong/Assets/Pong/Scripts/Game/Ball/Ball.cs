8f4194eaaebca46b2c21623743cf48787967f8ba
Hiroshi Miyazawa <gdgdkuro@gmail.com>
2020-05-11 11:48:51 +0900

    ballを追加

---
+++ Pong/Assets/Pong/Scripts/Game/Ball/Ball.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    public GameObject ball;
+    public Rigidbody2D rbody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        UnityEngine.Assertions.Assert.IsNotNull(ball);
+        rbody = ball.GetComponent<Rigidbody2D>();
+        UnityEngine.Assertions.Assert.IsNotNull(rbody);
+		// 回転をロック
+        rbody.constraints = RigidbodyConstraints2D.FreezeRotation;
+		// 左進行
+        rbody.velocity = new Vector2(-2.0f, 0.0f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

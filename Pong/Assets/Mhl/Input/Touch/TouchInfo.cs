300d12dad134b366b8d90529a184e9f08774a029
Hiroshi Miyazawa <gdgdkuro@gmail.com>
2020-05-19 15:59:18 +0900

    タッチ情報クラスを追加

---
+++ Pong/Assets/Mhl/Input/Touch/TouchInfo.cs
@@ -0,0 +1,127 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Mhl
+{
+    /// <summary>
+    /// タッチ情報クラス
+    /// </summary>
+    public class TouchInfo
+    {
+        /// <summary>
+        /// タッチ状態定義
+        /// </summary>
+        public enum Status
+        {
+            None,       // 何もなし
+            Began,      // タッチ開始
+            Moved,      // ドラッグ中
+            Stationaly, // ドラッグ中で移動なし
+            Ended,      // タッチ終了
+            Canceled    // キャンセル
+        };
+        private static readonly int InvalidTouchId = -1; // 無効なタッチID
+
+
+        // タッチID
+        private int touchId = InvalidTouchId;
+        // タッチ位置
+        private Vector3 position = new Vector3(0, 0, 0);
+        // ステータス
+        private Status status = Status.None;
+
+        public int TouchId
+        {
+            set { touchId = value; }
+            get { return touchId; }
+        }
+        public Vector3 Position
+        {
+            set { position = value; }
+            get { return position; }
+        }
+        public Status TouchStatus
+        {
+            set { status = value; }
+            get { return status; }
+        }
+
+        public TouchInfo()
+        {
+            Clear();
+        }
+
+        public void Clear()
+        {
+            touchId = InvalidTouchId;
+            position = Vector3.zero;
+            status = Status.None;
+        }
+
+        public void Copy(TouchInfo source)
+        {
+            touchId = source.touchId;
+            position = source.position;
+            status = source.status;
+        }
+
+        public bool EqualPosition(TouchInfo source)
+        {
+            if ((position.x == source.position.x)
+                && (position.y == source.position.y)
+                && (position.z == source.position.z))
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public bool Equals(TouchInfo source)
+        {
+            if ((touchId == source.touchId)
+                && (position.x == source.position.x)
+                && (position.y == source.position.y)
+                && (position.z == source.position.z)
+                && (status == source.status))
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public bool IsTouchIdInvalid()
+        {
+            return IsTouchIdInvalid(touchId);
+        }
+
+        public static bool IsTouchIdInvalid(int id)
+        {
+            if (id == InvalidTouchId)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public static string ToStatusString(TouchInfo.Status touchStatus)
+        {
+            return touchStatus switch
+            {
+                Status.None => "None",
+                Status.Began => "Began",
+                Status.Moved => "Moved",
+                Status.Stationaly => "Stationaly",
+                Status.Ended => "Ended",
+                Status.Canceled => "Canceled",
+                _ => "Unknown",
+            };
+        }
+
+        public void Print()
+        {
+            Debug.Log(string.Format("id={0} x={1} y={2} z={3} status={4}",
+                touchId, position.x, position.y, position.z, ToStatusString(status)));
+        }
+    }
+}

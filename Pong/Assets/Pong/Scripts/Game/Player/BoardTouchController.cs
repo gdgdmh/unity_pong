90539c1a1ab979caa1d3b5ca1c3b9fb892bd4139
Hiroshi Miyazawa <gdgdkuro@gmail.com>
2020-05-19 12:15:04 +0900

    ボード操作IFを追加

---
+++ Pong/Assets/Pong/Scripts/Game/Player/BoardTouchController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Pong
+{
+    /// <summary>
+    /// ボードタッチ操作クラス
+    /// </summary>
+    public class BoardTouchController : IBoardControllable
+    {
+        /// <summary>
+        /// 移動
+        /// </summary>
+        public void MoveBoard()
+        {
+        }
+    }
+}
---
+++ Pong/Assets/Pong/Scripts/Game/Player/IBoardControllable.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Pong
+{
+    /// <summary>
+    /// ボード操作インターフェース
+    /// </summary>
+    public interface IBoardControllable
+    {
+        /// <summary>
+        /// 移動
+        /// </summary>
+        void MoveBoard();
+    }
+}

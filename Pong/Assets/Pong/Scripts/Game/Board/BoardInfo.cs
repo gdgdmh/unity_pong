e70007990cbb075f0b79f5aa402c0cbcb61b9ddd
Hiroshi Miyazawa <gdgdkuro@gmail.com>
2020-05-21 14:28:28 +0900

    板情報クラスを追加

---
+++ Pong/Assets/Pong/Scripts/Game/Board/BoardInfo.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Pong
+{
+    /// <summary>
+    /// 板情報
+    /// </summary>
+    public class BoardInfo
+    {
+        private float height; // 板の縦幅
+        private Vector3 position; // 板の位置
+
+        public float Height
+        {
+            get { return height; }
+        }
+        public Vector3 Position
+        {
+            get { return position; }
+        }
+
+        public BoardInfo(float height, Vector3 position)
+        {
+            this.height = height;
+            this.position = position;
+        }
+    }
+}

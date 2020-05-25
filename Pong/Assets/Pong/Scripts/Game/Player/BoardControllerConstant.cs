85ad0451be94361ffd7ce416376e1de3622bd8db
Hiroshi Miyazawa <gdgdkuro@gmail.com>
2020-05-25 14:41:09 +0900

    ボード作成インターフェースクラスを追加 ボードコントローラー定数定義クラスを追加

---
+++ Pong/Assets/Pong/Scripts/Game/Player/BoardControllerConstant.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Pong
+{
+    /// <summary>
+    /// ボードコントローラー定数定義クラス
+    /// </summary>
+    public static class BoardControllerConstant
+    {
+        // ボードコントローラーのタイプ
+        public enum Type : int
+        {
+            Touch,  // タッチ操作
+            Cpu1    // CPU1
+        };
+    }
+}
---
+++ Pong/Assets/Pong/Scripts/Game/Player/IBoardControllerCreatable.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Pong
+{
+    /// <summary>
+    /// ボードコントローラー作成インターフェース
+    /// </summary>
+    public interface IBoardControllerCreatable
+    {
+        /// <summary>
+        /// ボードコントローラー作成
+        /// </summary>
+        /// <param name="type">コントローラーのタイプ</param>
+        /// <returns>作成したコントローラー</returns>
+        Pong.IBoardControllable Create(Pong.BoardControllerConstant.Type type);
+    }
+}

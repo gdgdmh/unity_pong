bdc402d1d5192ec88845da1bb7af4310a8e88cbb
Hiroshi Miyazawa <gdgdkuro@gmail.com>
2020-05-29 12:04:21 +0900

    メインメニューのインターフェースを追加 メインメニュープレイヤーモード選択を追加

---
+++ Pong/Assets/Pong/Scripts/Mainmenu/MainmenuConstant.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Pong
+{
+    public class MainmenuConstant
+    {
+        public enum Type : int
+        {
+            None,               // なし
+            PlayerModeSelect,   // プレイヤーモード選択
+            PvcCpuLevelSelect,  // CPUレベル選択(PlayerVsCpu)
+            CvcCpuLevelSelect1, // CPUレベル選択(CpuVsCpu1人目)
+            CvcCpuLevelSelect2, // CPUレベル選択(CpuVsCpu2人目)
+        };
+    }
+}
---
+++ Pong/Assets/Pong/Scripts/Mainmenu/MainmenuTask.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Pong
+{
+    public class MainmenuTask : MonoBehaviour
+    {
+        //private int scene;
+        private Pong.IMainmenuSelectable mainmenu;
+        private Pong.MainmenuConstant.Type type;
+
+        public MainmenuTask()
+        {
+            type = Pong.MainmenuConstant.Type.PlayerModeSelect;
+            //scene = 0;
+        }
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            if (type == Pong.MainmenuConstant.Type.PlayerModeSelect)
+            {
+                SetPlayerSelectPrefab();
+            }
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (mainmenu == null)
+            {
+                return;
+            }
+            UpdatePlayerSelectPrefab();
+
+        }
+
+        private void SetPlayerSelectPrefab()
+        {
+            GameObject prefab = (GameObject)Resources.Load("Prefabs/Mainmenu/mainmenu_player_select");
+            UnityEngine.Assertions.Assert.IsNotNull(prefab);
+            GameObject g = Instantiate(prefab);
+            UnityEngine.Assertions.Assert.IsNotNull(g);
+
+            mainmenu = (Pong.IMainmenuSelectable)g.GetComponent<Pong.MainmenuPlayerModeSelect>();
+            UnityEngine.Assertions.Assert.IsNotNull(g);
+        }
+
+        private void UpdatePlayerSelectPrefab()
+        {
+            if (mainmenu.GetTransitionType() == MainmenuConstant.Type.None)
+            {
+                // 何もしない
+                return;
+            }
+
+            if (mainmenu.GetTransitionType() == MainmenuConstant.Type.PvcCpuLevelSelect)
+            {
+                Debug.Log("PvC");
+                return;
+            }
+            if (mainmenu.GetTransitionType() == MainmenuConstant.Type.CvcCpuLevelSelect1)
+            {
+                Debug.Log("CvC");
+                return;
+            }
+        }
+    }
+}
